92cc260876e5b27c2c67c85be62ed44710c02ec5
Antonio Muniz <antonio.gustavo_@live.com>
2021-03-13 20:16:56 -0300

    Made some conversions to run code in python 3

--- Semana 2 - Sockets/exercicio2/server.py
+++ Semana 2 - Sockets/exercicio2/server.py
@@ -11,31 +11,37 @@ secondNumber = 0
 def createResult(firstNumber, request, secondNumber):
     message = ""
     if request == "1":
-        message = ("Ok, you want to make the operation %d + %d \n So, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber + secondNumber)))
+        message = ("Ok, you want to make the operation %d + %d \nSo, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber + secondNumber)))
     elif request == "2":
-        message = ("Ok, you want to make the operation %d - %d \n So, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber - secondNumber)))
+        message = ("Ok, you want to make the operation %d - %d \nSo, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber - secondNumber)))
     elif request == "3":
         message = ("Ok, you want to make the operation %d / %d \n So, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber / secondNumber)))
     elif request == "4":
-        message = ("Ok, you want to make the operation with %d * %d \n So, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber * secondNumber)))
+        message = ("Ok, you want to make the operation with %d * %d \nSo, that's the result %d\n" %( firstNumber, secondNumber, (firstNumber * secondNumber)))
+    else:
+        message = ("Ok, you want to make the operation with %d <i have no idea what operation is that> %d \nSo, that's the result %d\n" %(firstNumber, secondNumber, 0 ))
     return message.encode()
 
 while True:
     connectionSocket, addr = serverSocket.accept()
-    connectionSocket.send("Hey man, How are you? \nI'm here to solve math questions. \nPlease insert two numbers!\n")
+    connectionSocket.send( ("Hey man, How are you? \nI'm here to solve math questions. \nPlease insert two numbers!\n").encode() )
 
     while True:
         request = connectionSocket.recv(1024).decode()
 
-        if len(request) >= 3:
-            numbers = request.split(' ')
-            firstNumber = (float(numbers[0]))
-            secondNumber = (float(numbers[1]))
-            message = ("Ok, you want to make some operation with %d and %d \n Now, insert the operation\n" %( firstNumber, secondNumber))
-            connectionSocket.send(message.encode())
-        elif len(request) == 1:
-            connectionSocket.send(createResult(firstNumber, request, secondNumber))
+        if not request: 
             break
+        
+        else:
+            if len(request) >= 3:
+                numbers = request.split(' ')
+                firstNumber = (float(numbers[0]))
+                secondNumber = (float(numbers[1]))
+                message = ("Ok, you want to make some operation with %d and %d \n Now, insert the operation\n" %( firstNumber, secondNumber))
+                connectionSocket.send(message.encode())
+            elif len(request) == 1:
+                connectionSocket.send(createResult(firstNumber, request, secondNumber))
+                break
 
     connectionSocket.close()
 
